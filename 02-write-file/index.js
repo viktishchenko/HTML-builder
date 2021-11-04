375c0ba6e0a2c43c4ddbcbe852b207db5255ade0
Viktor Tishchenko <9043258451vvt@gmail.com>
2021-11-04 15:24:52 +0600

    feat: add solusion

---
+++ 02-write-file/index.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+const path = require("path");
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+fs.writeFile(path.join(__dirname, "text.txt"), "", (err) => {
+  if (err) throw err;
+});
+
+const exitTheProcess = () => {
+  process.stdout.write("- See ya, bye-bye!");
+  process.exit();
+};
+
+rl.output.write("Hi there! Please type something ...\n");
+rl.on("line", (data) => {
+  if (data.trim() === "exit") exitTheProcess();
+
+  fs.appendFile(path.join(__dirname, "text.txt"), `${data} `, function (error) {
+    if (error) throw error;
+  });
+});
+
+rl.on("SIGINT", () => {
+  exitTheProcess();
+});
