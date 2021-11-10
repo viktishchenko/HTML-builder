7f35204d799c50929b2a2113e63f85fc85cf29cc
Viktor Tishchenko <9043258451vvt@gmail.com>
2021-11-10 10:39:24 +0600

    fix: 01 task change file name

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
 const path = require("path");
 const stdout = process.stdout;
-const stream = new fs.ReadStream(path.join(__dirname, "text1.txt"), "utf-8");
+const stream = new fs.ReadStream(path.join(__dirname, "text.txt"), "utf-8");
 stream.on("data", (Data) => stdout.write(Data));
