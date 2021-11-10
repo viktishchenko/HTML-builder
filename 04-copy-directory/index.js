67084ea05aff948f2a8f13f1d3b053aa4ce0d837
Viktor Tishchenko <9043258451vvt@gmail.com>
2021-11-10 15:10:34 +0600

    fix: add async rm function to 04-th task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -4,6 +4,43 @@ const path = require("path");
 const filesPath = path.join(__dirname, "files");
 const fileCopyPath = path.join(__dirname, "file-copy");
 
+const removeDir = async (fileCopyPath) => {
+  await fs.promises.rm(fileCopyPath, { force: true, recursive: true });
+};
+
+const copyRecursiveAsync = async function (src, dest) {
+  const stats = await fs.promises.stat(src, { withFileTypes: true });
+  if (stats.isDirectory()) {
+    await fs.promises.mkdir(dest, { recursive: true });
+    const files = await fs.promises.readdir(src, { withFileTypes: true });
+    files.forEach((file) => {
+      copyRecursiveAsync(
+        path.join(src, `${file.name}`),
+        path.join(dest, `${file.name}`)
+      );
+    });
+  } else {
+    await fs.promises.copyFile(src, dest);
+  }
+};
+
+const copyDirectory = async (src, dist) => {
+  await removeDir(fileCopyPath);
+  await copyRecursiveAsync(filesPath, fileCopyPath);
+};
+
+copyDirectory(filesPath, fileCopyPath);
+
+/**
+ * This is old version with no remove function and promise
+ */
+
+/* const fs = require("fs");
+const path = require("path");
+
+const filesPath = path.join(__dirname, "files");
+const fileCopyPath = path.join(__dirname, "file-copy");
+
 const copyRecursiveAsync = function (src, dest) {
   fs.stat(src, { withFileTypes: true }, (err, stats) => {
     if (err) throw err;
@@ -32,4 +69,4 @@ const copyRecursiveAsync = function (src, dest) {
   });
 };
 
-copyRecursiveAsync(filesPath, fileCopyPath);
+copyRecursiveAsync(filesPath, fileCopyPath); */
