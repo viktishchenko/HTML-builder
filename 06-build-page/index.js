937dbdad61bab6a1b8ac44952672dbcf04f69b97
Viktor Tishchenko <9043258451vvt@gmail.com>
2021-11-10 15:24:58 +0600

    fix: add remove function

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -2,6 +2,10 @@ const fs = require("fs");
 const path = require("path");
 
 async function startModuleBuilder() {
+  await fs.promises.rm(path.join(__dirname, "project-dist"), {
+    recursive: true,
+  });
+
   await fs.promises.mkdir(path.join(__dirname, "project-dist"), {
     recursive: true,
   });
