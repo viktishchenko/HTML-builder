5d83c2cfd7f9306bdb013688a3ee0dcfef666e8d
Viktor Tishchenko <9043258451vvt@gmail.com>
2022-04-19 13:29:42 +0600

    feat: add logger service with simple dependency injection

--- 10-flowers-api/src/app.ts
+++ 10-flowers-api/src/app.ts
@@ -1,15 +1,19 @@
 import express, { Express } from "express";
 import { Server } from "http";
 import { userRouter } from "./users/users";
+import { LoggerService } from "./logger/logger.service";
+import { Logger } from "tslog";
 
 export class App {
   app: Express;
   server: Server;
   port: number;
+  logger: LoggerService;
 
-  constructor() {
+  constructor(logger: LoggerService) {
     this.app = express();
     this.port = 8000;
+    this.logger = logger;
   }
 
   useRoutes() {
@@ -19,6 +23,6 @@ export class App {
   public async init() {
     this.useRoutes();
     this.server = this.app.listen(this.port);
-    console.log(`Сервер запущен на реез://localhost:${this.port}`);
+    this.logger.log(`Сервер запущен на http://localhost:${this.port}`);
   }
 }
